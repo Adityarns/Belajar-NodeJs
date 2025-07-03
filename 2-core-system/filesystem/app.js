b5e66c1189baaa362c229abfa76b2476586cdacc
Aditya Rahman Syach <adityarachmansyach@gmail.com>
2025-07-03 17:26:44 +0700

    Belajar mengenai coremodule yaitu filesystem(membaca dan menulis data ke file) serta readline(membuat prompt pertanyaan di cmd)

---
+++ 2-core-system/filesystem/app.js
@@ -0,0 +1,45 @@
+// menulis dan membaca data di file
+const fs = require('fs');
+// console.log(fs);
+
+
+// MENULIS SECARA SYNCHRONUS
+// try{
+//     fs.writeFileSync('anjay/ea.txt', "Halo dek ini synchronus");
+// } catch(e){
+//     console.log(e);
+// }
+
+// MENULIS SECARA ASYNCHRONUS
+// fs.writeFile('ui.txt', "Halo dek ini asynchronus", (e) => {
+//     console.log(e);
+// });
+
+
+// MEMBACA ISI SYNCHRONUS
+// const data = fs.readFileSync('data/ea.txt', 'utf-8');
+// console.log(data);
+
+//  MEMBACA ISI AYNCHRONUS
+// fs.readFile('data/ui.txt','utf-8', (e, data)=>{
+//     if(e) throw e;
+//     console.log(data);
+// })
+
+
+// membuat prompt pertanyaan di cmd
+const readline = require('readline');
+const rl = readline.createInterface ({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question('Masukkan nama anda: ', (nama)=>{
+    rl.question('Masukkan nomor anda: ', (nomor)=>{
+        fs.writeFile('ai.txt', `${nama}, ${nomor}`, (e)=>{
+            console.log(e);
+        })
+        console.log(`terima kasih ${nama} dengan nomor ${nomor}`)
+        rl.close();
+    })
+})
