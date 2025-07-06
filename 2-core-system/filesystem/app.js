97b19aa52045416ce02564d32f3ee9537be15552
Aditya Rahman Syach <adityarachmansyach@gmail.com>
2025-07-06 14:44:58 +0700

    memperbaiki kode node

--- 2-core-system/filesystem/app.js
+++ 2-core-system/filesystem/app.js
@@ -1,5 +1,5 @@
 // menulis dan membaca data di file
-const fs = require('fs');
+// const fs = require('fs');
 // console.log(fs);
 
 
@@ -28,18 +28,73 @@ const fs = require('fs');
 
 
 // membuat prompt pertanyaan di cmd
+// const readline = require('readline');
+// const rl = readline.createInterface ({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
+
+// rl.question('Masukkan nama anda: ', (nama)=>{
+//     rl.question('Masukkan nomor anda: ', (nomor)=>{
+//         fs.writeFile('ai.txt', `${nama}, ${nomor}`, (e)=>{
+//             console.log(e);
+//         })
+//         console.log(`terima kasih ${nama} dengan nomor ${nomor}`)
+//         rl.close();
+//     })
+// })
+
+
+// rl.question('Masukkan nama anda: ', (nama)=>{
+//     rl.question('Masukkan nomor anda: ', (nomor)=>{
+//         fs.writeFile('ai.txt', `${nama}, ${nomor}`, (e)=>{
+//             console.log(e);
+//         })
+//         console.log(`terima kasih ${nama} dengan nomor ${nomor}`)
+//         rl.close();
+//     })
+// })
+
 const readline = require('readline');
-const rl = readline.createInterface ({
+const fs = require('fs');
+
+const dirPath = './data';
+if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+}
+
+const dataPath = 'data/contacts.json';
+if (!fs.existsSync(dataPath)) {
+    fs.writeFileSync(dataPath, '[]', 'utf-8');
+}
+
+const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 
-rl.question('Masukkan nama anda: ', (nama)=>{
-    rl.question('Masukkan nomor anda: ', (nomor)=>{
-        fs.writeFile('ai.txt', `${nama}, ${nomor}`, (e)=>{
-            console.log(e);
-        })
-        console.log(`terima kasih ${nama} dengan nomor ${nomor}`)
-        rl.close();
-    })
-})
+const tulisPertanyaan = (pertanyaan) => {
+    return new Promise((resolve, reject) => {
+        rl.question(pertanyaan, (jawaban) => {
+            resolve(jawaban);
+        });
+    });
+};
+
+const main = async () => {
+    const nama = await tulisPertanyaan("Masukkan nama anda: ");
+    const nomor = await tulisPertanyaan("Masukkan nomor anda: ");
+    const email = await tulisPertanyaan("Masukkan email anda: ");
+    
+    const contact = { nama, nomor, email };
+    const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
+    const contacts = JSON.parse(fileBuffer);
+    contacts.push(contact);
+    
+    fs.writeFileSync('data/contacts.json', JSON.stringify(contacts, null, 2));
+    console.log("Data berhasil disimpan!");
+
+    rl.close();
+};
+
+main();
