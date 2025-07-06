a6797e2ed0203e7bc3b658351b62515d3df0f762
Aditya Rahman Syach <adityarachmansyach@gmail.com>
2025-07-06 13:56:07 +0700

    mencoba beberapa package

--- 3-NPM/main.js
+++ 3-NPM/main.js
@@ -1,3 +1,9 @@
-console.log("hello world!");
 // Gunakan "npm i validator untuk install"
 // Gunakan "npm uninstall validator untuk uninstall"
+
+const validator = require("validator");
+const chalk = require("chalk");
+
+// console.log(validator.isEmail("adityarachmansyach@gmail.com"));
+console.log(validator.isMobilePhone("082113467640", 'id-ID'));
+console.log(validator.isNumeric("12352356135"));
