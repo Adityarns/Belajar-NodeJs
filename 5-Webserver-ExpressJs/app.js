40878feb7fa5742c6904372d9b5afdd01c6310bc
Aditya Rahman Syach <adityarachmansyach@gmail.com>
2025-07-08 12:13:05 +0700

    menambahkan framework ejs

--- 5-Webserver-ExpressJs/app.js
+++ 5-Webserver-ExpressJs/app.js
@@ -1,18 +1,40 @@
 const express = require('express')
+const expressLayouts = require('express-js-layouts')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs')
+app.use(expressLayouts)
+
 app.get( '/', (req, res) =>{
-    res.send("Hello Adit")
+    const mahasiswa = [
+    {
+        nama: 'Aditya Rahman Syach',
+        email: 'aditya@gmail.com'
+    },
+    {
+        nama: 'Ilyasa Sidra Thoriq',
+        email: 'Arap@gmail.com'    
+    },
+    {
+        nama: 'Ahmad Ipul',
+        email: 'Ahmad@gmail.com'    
+    },
+    ]
+    res.render('index' , {
+        title: 'Halaman utama',
+        nama: 'Adityarns_',
+        mahasiswa 
+    } )    
+    
 })
 
 app.get( '/about', (req, res) =>{
-    // res.send("Ini About")
-    res.sendFile('./about.html', {root: __dirname})
+    res.render('about', {title: 'Halaman About'})    
 })
 
 app.get( '/contact', (req, res) =>{
-    res.send("Ini contact")
+    res.render('contact', {title: 'Halaman Contact'})    
 })
 
 app.use( '/', (req, res) =>{
