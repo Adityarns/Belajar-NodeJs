8493cf66b73361c3bcb0542d68be480bd0f00b64
Aditya Rahman Syach <adityarachmansyach@gmail.com>
2025-07-09 15:32:23 +0700

    menyatukan beberapa file view(header,dll) menjadi satu file yaitu main-layouts. serta menggunakan express-ejs-layouts agar lebih simpel syntaxnya

---
+++ 6-Middleware-ExpressJs/app.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+app.use(expressLayouts)
+
+
+app.use(express.static('public'))
+
+
+app.use((req, res, next)=>{
+    console.log('Time: ', Date.now())
+    next()
+})
+
+app.get( '/', (req, res) =>{
+    const mahasiswa = [
+    {
+        nama: 'Aditya Rahman Syach',
+        email: 'aditya@gmail.com'
+    },
+    {
+        nama: 'Ilyasa Sidra Thoriq',
+        email: 'Arap@gmail.com'    
+    },
+    {
+        nama: 'Ahmad Ipul',
+        email: 'Ahmad@gmail.com'    
+    },
+    ]
+    res.render('index' , {
+        layout: 'layouts/main-layouts',
+        title: 'Halaman utama',
+        nama: 'Adityarns_',
+        mahasiswa 
+    } )    
+    
+})
+
+app.get( '/about', (req, res) =>{
+    res.render('about', {
+        layout: 'layouts/main-layouts',
+        title: 'Halaman About'})    
+})
+
+app.get( '/contact', (req, res) =>{
+    res.render('contact', {
+        layout: 'layouts/main-layouts',        
+        title: 'Halaman Contact'})    
+})
+
+app.use( '/', (req, res) =>{
+    res.status(404)
+    res.send("404")
+})
+
+app.listen(port, ()=>{
+    console.log(`Sedang menjalankan di localHost ${port}`)
+})
